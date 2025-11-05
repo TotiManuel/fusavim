4f77f13ff3d04c03b23f2430d667a4fb37d2ee5e
unknown <TotiManuel>
2025-11-05 05:34:16 -0300

    Minimo cambio en Requisitos

--- src/pages/Requisitos.tsx
+++ src/pages/Requisitos.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { jsPDF } from "jspdf";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -129,7 +129,9 @@ export default function Requisitos() {
 
     setError("");
 };
-
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div className="min-h-screen">
       <Header />
